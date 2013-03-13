7fe904c74e8dec5493288d10c1adf783ee89471c
Prabin Deka <pdeka@thoughtworks.com>
2013-03-13 23:56:16 +1100

    Prabin - Keep a find by in the home page to talk to.

--- src/system-tests/java/com/example/video/web/page/HomePage.java
+++ src/system-tests/java/com/example/video/web/page/HomePage.java
@@ -1,13 +1,15 @@
 package com.example.video.web.page;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.containsString;
 
 public class HomePage implements Page {
 
-    private WebElement search;
+//    @FindBy(className = "someClassName")
+//    private WebElement search;
 
     public void verify(Browser browser) {
         assertThat("Unexpected page", browser.getBodyClass(), containsString("home"));
